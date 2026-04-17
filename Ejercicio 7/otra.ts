e2b279139b7fde4af75221bf8804475d826a72e9
Agustin Lupi <agustinlupi36@gmail.com>
2026-04-17 16:49:23 -0300

    Update otra.ts

--- Ejercicio 7/otra.ts
+++ Ejercicio 7/otra.ts
@@ -1,4 +1,4 @@
 const AB = Number(prompt("Ingrese el valor del cateto AB"))
 const BC = Number(prompt("Ingrese el valor del cateto BC"))
 const area = (AB * BC) / 2
-alert("El area del triangulo es: " + area)
+alert("El area del triangulo es: " + area + " cm^2")
