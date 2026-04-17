1490f1867f01e93c6ea72b66d7637bd312fccad4
Agustin Lupi <agustinlupi36@gmail.com>
2026-04-17 17:32:31 -0300

    Delete ejercicio 2 directory

--- ejercicio 2/otra.ts
+++
@@ -1,4 +0,0 @@
-let juego = prompt("Cual es el juego que quieres comprar")
-const precio = Number(prompt("Ingrese el precio del producto"))
-const IVA = precio * 1.21
-alert("El precio de " + juego + " es: $" + IVA)
