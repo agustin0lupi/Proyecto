a13cfa2ed712ae4a4634fa9be903a2602870e2db
Agustin Lupi <agustinlupi36@gmail.com>
2026-04-20 15:18:30 -0300

    Add files via upload

---
+++ ejercicio 2/otra.ts
@@ -0,0 +1,4 @@
+const velocidad = Number(prompt("¿Cual es la velocidad inicial del vehiculo?"))
+const distancia = Number(prompt("¿Cual es la distancia que recorre el vehiculo?"))
+const tiempo = distancia / velocidad
+alert("El tiempo por la que el vehiculo recorre [x] distancia es de " + tiempo + " horas")
